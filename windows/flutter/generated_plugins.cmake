8d2103943c6960e95b894d6f6703760381bb54fb
Pranav Pandey <pranavpandey1998official@gmail.com>
2026-06-16 20:08:16 +0530

    fix: build issues

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,7 +6,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   flutter_gemma
   flutter_secure_storage_windows
-  gal
   isar_community_flutter_libs
   share_plus
   url_launcher_windows
